6ecb7746911b00348b695fcda00a9b6514e5c671
cdefg666 <791150351@qq.com>
2023-05-25 19:28:46 +0800

    feat: use the reference of the did as chainId to switch network

--- src/hooks/useIdentity.ts
+++ src/hooks/useIdentity.ts
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useContext } from "react";
 import { CRYPTO_WALLET } from "@dataverse/runtime-connector";
 import { Context } from "../main";
 import app from "../../output/app.json";
+import { getNamespaceAndReferenceFromDID } from "../utils";
 
 export function useIdentity() {
   const [did, setDid] = useState("");
@@ -23,23 +24,42 @@ export function useIdentity() {
     return res;
   };
 
-  const switchNetwork = async () => {
-    const res = await runtimeConnector.switchNetwork(137);
+  const switchNetwork = async (chainId: number) => {
+    const res = await runtimeConnector.switchNetwork(chainId);
+    return res;
+  };
+
+  const checkIsCurrentDIDValid = async (params: { appName: string }) => {
+    const res = await runtimeConnector.checkIsCurrentDIDValid(params);
     return res;
   };
 
   const connectIdentity = async () => {
+    const currentDID = await runtimeConnector.getCurrentDID();
+    let chainId;
+    try {
+      const { reference } = getNamespaceAndReferenceFromDID(currentDID);
+      chainId = Number(reference);
+    } catch (error) {}
+
     const currentWallet = await getCurrentWallet();
     if (!currentWallet) {
       const wallet = await chooseWallet();
       await connectWallet(wallet);
     }
-    await switchNetwork();
-    const did = await runtimeConnector.connectIdentity({
-      appName: app.createDapp.name,
-    });
-    setDid(did);
-    return did;
+
+    const res = await checkIsCurrentDIDValid({ appName: app.createDapp.name });
+    if (!res) {
+      await switchNetwork(chainId || 137);
+      const did = await runtimeConnector.connectIdentity({
+        appName: app.createDapp.name,
+      });
+      setDid(did);
+      return did;
+    }
+
+    setDid(currentDID);
+    return currentDID;
   };
 
   return {
--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,8 @@
-import fs from "fs";
-import path from "path";
-
 export function getAddressFromDid(did: string) {
   return did.slice(did.lastIndexOf(":") + 1);
 }
 
-function generateRandomString(length: number): string {
+export function generateRandomString(length: number): string {
   let result = '';
   const characters = 'abcdefghijklmnopqrstuvwxyz';
   const charactersLength = characters.length;
@@ -13,4 +10,15 @@ function generateRandomString(length: number): string {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
   return result;
+}
+
+export function getNamespaceAndReferenceFromDID(did: string) {
+  const res = did.match('did:pkh:([-a-z0-9]{3,8}):([-_a-zA-Z0-9]{1,32})');
+  if (!res) {
+    throw new Error('Not in pkhDID format');
+  }
+  return {
+    namespace: res[1],
+    reference: res[2],
+  };
 }
